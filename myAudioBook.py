cb5f7020d6c81d2083dc923fb80a49799382a2c0
Pablo Molina <seriusmail13@gmail.com>
2023-08-02 12:30:35 +0200

    Uploading the audiobook proyect WIP

---
+++ myAudioBook.py
@@ -0,0 +1,50 @@
+# For the GUI
+import tkinter as tk
+import tkinter.filedialog as fd
+# To read the epub
+import epub2txt as epub
+# To voice the epub
+import pyttsx3
+
+root = tk.Tk()
+root.title('Narrator')
+root.geometry('400x300')
+
+file = []
+
+def select_file():
+    file_path = fd.askopenfilename()
+    file.append(file_path)
+    if file_path == '':
+        print ('Error: No file selected')
+
+def convert():
+    text = epub.epub2txt(file[0])
+    engine = pyttsx3.init()
+    engine.save_to_file(text,'NewAudioBook.mp3')
+    file = []
+    engine.runAndWait()
+
+def read():
+    text = epub.epub2txt(file[0])
+    engine = pyttsx3.init()
+    engine.say(text)
+    
+def stop():
+    engine = pyttsx3.init()
+    engine.stop()
+    
+# Buttons
+tk.Button(text= ('Open'),
+          command= select_file).pack(fill= tk.X)
+
+tk.Button(text= ('Convert'),
+          command= convert).pack(fill=tk.X)
+
+tk.Button(text= ('Read'),
+          command= read).pack(fill=tk.X)
+
+tk.Button(text= ('Stop'),
+          command= stop, fg='red').pack(fill=tk.X)
+
+tk.mainloop()
